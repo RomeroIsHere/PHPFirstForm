7e0b3e4f49bba3a0582105d2ffc51bbf5f7eb9e4
Guillermo Romero Cuevas <guillermoromecu@gmail.com>
2025-10-21 15:46:43 -0600

    Finished SideBar Functionality

--- Examen/scripts/Sidebar.js
+++ Examen/scripts/Sidebar.js
@@ -3,14 +3,9 @@ let SideBarId="sidebar-multi-level-sidebar"
 let SidebarObject=document.getElementById(SideBarId);
 function toggleSideBar(){
 // sidebar-multi-level-sidebar
-    SideBarState=!SideBarState
-    console.log(SideBarState)
-    if(SideBarState){
-        SidebarObject.style.width = "64";
-        // SidebarObject.style.visibility="hidden"
-        
-    }else{
-        SidebarObject.style.width = "0";
-        // SidebarObject.style.visibility="visible"
-    }
+    SidebarObject.style.display="Block";
+}
+
+function closeSideBar(){
+    SidebarObject.style.display="none";
 }
