829bc3037960d3d8bdb82df67ac43843c45fed97
Guillermo Romero Cuevas <guillermoromecu@gmail.com>
2025-10-21 17:02:27 -0600

    Prettified

--- Examen/scripts/Sidebar.js
+++ Examen/scripts/Sidebar.js
@@ -1,11 +1,11 @@
-let SideBarState=false;
-let SideBarId="sidebar-multi-level-sidebar"
-let SidebarObject=document.getElementById(SideBarId);
-function toggleSideBar(){
-// sidebar-multi-level-sidebar
-    SidebarObject.style.display="Block";
+let SideBarState = false;
+let SideBarId = "sidebar-multi-level-sidebar";
+let SidebarObject = document.getElementById(SideBarId);
+function toggleSideBar() {
+    // sidebar-multi-level-sidebar
+    SidebarObject.style.display = "Block";
 }
 
-function closeSideBar(){
-    SidebarObject.style.display="none";
+function closeSideBar() {
+    SidebarObject.style.display = "none";
 }
